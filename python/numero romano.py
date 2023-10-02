057bb5062f8da054f5974969c04fe8eb78ccb1e6
MatiasSeleme <132406956+MatiasSeleme@users.noreply.github.com>
2023-10-01 23:32:23 -0300

    Update numero romano.py

--- python/numero romano.py
+++ python/numero romano.py
@@ -1,17 +1,11 @@
-def Romano_decimal(numero):
-    
-    valor = { 'i' : 1, 'v' : 5, 'x' : 10, 'l': 50, 'c' : 100, 'd' : 500, 'm' : 1000 }
+valor = {'i': 1, 'v': 5, 'x': 10, 'l': 50, 'c': 100, 'd': 500, 'm': 1000}
 
-    decimal = 0
-    anterior = 0
-    for size in numero[::-1]:
-        Va = valor[size]
-        if Va < anterior:
-            decimal -=Va
-        else:
-            decimal+=Va
+def numerosRomanos(numero):
+    if len(numero) == 1:
+        return valor[numero[0]]
+    elif valor[numero[0]] >= valor[numero[1]]:
+        return valor[numero[0]] + numerosRomanos(numero[1:])
+    else:
+        return -valor[numero[0]] + numerosRomanos(numero[1:])
 
-        anterior = Va    
-    return decimal
-
-print(Romano_decimal('iv'))
+print(numerosRomanos('xiv'))
