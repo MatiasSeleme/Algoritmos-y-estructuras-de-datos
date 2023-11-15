eb51b9fc64648a51f71ae1495f3bd80dadcb699e
MatiasSeleme <132406956+MatiasSeleme@users.noreply.github.com>
2023-11-14 21:18:39 -0300

    Pokemon_segundo_parcial.py

--- Segundo parcial/Pokemon_segundo_parcial.py
+++ Segundo parcial/Pokemon_segundo_parcial.py
@@ -1,118 +1,180 @@
-class Pokemon:
+class PokemonGeneracion:
     def __init__(self, nombre, numero, tipo):
         self.nombre = nombre
         self.numero = numero
         self.tipo = tipo
 
-class PokemonTree:
-    class Node:
-        def __init__(self, pokemon):
-            self.pokemon = pokemon
-            self.left = None
-            self.right = None
+    def __str__(self):
+        return f"{self.numero}: {self.nombre} ({', '.join(self.tipo)})"
 
-    def __init__(self, index):
+
+class PokemonGeneracionTree:
+    def __init__(self, key):
         self.root = None
-        self.index = index
+        self.key = key
 
-    def insertar_pokemon(self, pokemon):
-        new_node = self.Node(pokemon)
-        if self.index == 'nombre':
-            key = pokemon.nombre
-        elif self.index == 'numero':
-            key = pokemon.numero
-        elif self.index == 'tipo':
-            key = pokemon.tipo
-
-        if not self.root:
-            self.root = new_node
+    def insert_node(self, value):
+        if self.root is None:
+            self.root = TreeNode(value)
         else:
-            current = self.root
-            while True:
-                if self.index == 'nombre' and key.lower() < current.pokemon.nombre.lower():
-                    if current.left is None:
-                        current.left = new_node
-                        break
-                    current = current.left
-                elif self.index == 'nombre' and key.lower() > current.pokemon.nombre.lower():
-                    if current.right is None:
-                        current.right = new_node
-                        break
-                    current = current.right
-                elif self.index == 'numero' and key < current.pokemon.numero:
-                    if current.left is None:
-                        current.left = new_node
-                        break
-                    current = current.left
-                elif self.index == 'numero' and key > current.pokemon.numero:
-                    if current.right is None:
-                        current.right = new_node
-                        break
-                    current = current.right
-                elif self.index == 'tipo' and key < current.pokemon.tipo:
-                    if current.left is None:
-                        current.left = new_node
-                        break
-                    current = current.left
-                elif self.index == 'tipo' and key > current.pokemon.tipo:
-                    if current.right is None:
-                        current.right = new_node
-                        break
-                    current = current.right
+            self._insert(self.root, value)
 
-    def buscar_por_nombre(self, name):
-        name = name.lower()
-        current = self.root
-        while current:
-            if name == current.pokemon.nombre.lower() or current.pokemon.nombre.lower().startswith(name):
-                return current.pokemon
-            elif name < current.pokemon.nombre.lower():
-                current = current.left
+    def _insert(self, current_node, value):
+        if self.key(value) < self.key(current_node.value):
+            if current_node.left is None:
+                current_node.left = TreeNode(value)
             else:
-                current = current.right
-        return None
+                self._insert(current_node.left, value)
+        elif self.key(value) > self.key(current_node.value):
+            if current_node.right is None:
+                current_node.right = TreeNode(value)
+            else:
+                self._insert(current_node.right, value)
 
-    def obtener_tipo(self, type):
-        result = []
-        type = type.lower()
-        self._obtener_tipo(self.root, type, result)
-        return result
+    def inorden(self, current_node=None):
+        if current_node is None:
+            current_node = self.root
+        if current_node.left:
+            self.inorden(current_node.left)
+        print(current_node.value)
+        if current_node.right:
+            self.inorden(current_node.right)
 
-    def _obtener_tipo(self, node, type, result):
-        if node:
-            if node.pokemon.tipo.lower() == type:
-                result.append(node.pokemon.nombre)
-            self._obtener_tipo(node.left, type, result)
-            self._obtener_tipo(node.right, type, result)
+    def search_by_name_number(self, name, number):
+        return self._search_by_name_number(self.root, name, number)
 
-    def barrido_en_orden(self):
-        result = []
-        self._barrido_en_orden(self.root, result)
-        return result
+    def _search_by_name_number(self, current_node, name, number):
+        if current_node is None:
+            return None
+        if current_node.value.nombre.lower().startswith(name.lower()) and current_node.value.numero == number:
+            return current_node.value
+        if name.lower() < current_node.value.nombre.lower():
+            return self._search_by_name_number(current_node.left, name, number)
+        else:
+            return self._search_by_name_number(current_node.right, name, number)
 
-    def _barrido_en_orden(self, node, result):
-        if node:
-            self._barrido_en_orden(node.left, result)
-            result.append(node.pokemon.nombre)
-            self._barrido_en_orden(node.right, result)
+    def search_by_name_prefix(self, prefix):
+        matches = []
+        self._search_by_name_prefix(self.root, prefix.lower(), matches)
+        return matches
 
-tree_name = PokemonTree('nombre')
-tree_number = PokemonTree('numero')
-tree_type = PokemonTree('tipo')
+    def _search_by_name_prefix(self, current_node, prefix, matches):
+        if current_node is not None:
+            if current_node.value.nombre.lower().startswith(prefix):
+                matches.append(current_node.value)
+            if prefix < current_node.value.nombre.lower():
+                self._search_by_name_prefix(current_node.left, prefix, matches)
+            else:
+                self._search_by_name_prefix(current_node.right, prefix, matches)
 
-pokemon_data = [
-    {"nombre": "Bulbasaur", "numero": 1, "tipo": "planta"},
-    {"nombre": "Pikachu", "numero": 25, "tipo": "eléctrico"},
-    {"nombre": "Charmander", "numero": 4, "tipo": "fuego"},
-    {"nombre": "Squirtle", "numero": 7, "tipo": "agua"},
+    def get_names_by_type(self, pokemon_type):
+        names = []
+        self._get_names_by_type(self.root, pokemon_type, names)
+        return names
+
+    def _get_names_by_type(self, current_node, pokemon_type, names):
+        if current_node is not None:
+            if pokemon_type.lower() in [t.lower() for t in current_node.value.tipo]:
+                names.append(current_node.value.nombre)
+            self._get_names_by_type(current_node.left, pokemon_type, names)
+            self._get_names_by_type(current_node.right, pokemon_type, names)
+
+    def by_level(self):
+        levels = {}
+        self._by_level(self.root, 0, levels)
+        for level, values in sorted(levels.items()):
+            for value in sorted(values):
+                print(f'Nivel {level}: {value}')
+
+    def _by_level(self, current_node, level, levels):
+        if current_node is not None:
+            self._by_level(current_node.left, level + 1, levels)
+            if level not in levels:
+                levels[level] = []
+            levels[level].append(current_node.value.nombre)
+            self._by_level(current_node.right, level + 1, levels)
+
+    def search_specific_pokemons(self, pokemon_names):
+        for name in pokemon_names:
+            pokemon = self._search_by_name_prefix(self.root, name.lower(), [])
+            if pokemon:
+                print(pokemon)
+
+    def count_type_occurrences(self, types):
+        count = 0
+        for pokemon_type in types:
+            count += self._count_type_occurrences(self.root, pokemon_type.lower())
+        return count
+
+    def _count_type_occurrences(self, current_node, pokemon_type):
+        if current_node is None:
+            return 0
+        count = 0
+        if pokemon_type in [t.lower() for t in current_node.value.tipo]:
+            count += 1
+        count += self._count_type_occurrences(current_node.left, pokemon_type)
+        count += self._count_type_occurrences(current_node.right, pokemon_type)
+        return count
+
+
+class TreeNode:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+arbol_generaciones = PokemonGeneracionTree(key=lambda x: (x.nombre, x.numero, x.tipo))
+
+Pokemones = [
+    {'name': 'Bulbasaur', 'number': 1, 'type': ['Grass', 'Poison']},
+    {'name': 'Charmander', 'number': 4, 'type': ['Fire']},
+    {'name': 'Squirtle', 'number': 7, 'type': ['Water']},
+    {'name': 'Jolteon', 'number': 135, 'type': ['Electric']},
+    {'name': 'Lycanroc', 'number': 745, 'type': ['Rock']},
+    {'name': 'Tyrantrum', 'number': 697, 'type': ['Rock', 'Dragon']},
+    {'name': 'Venusaur', 'number': 3, 'type': ['Grass', 'Poison']},
+    {'name': 'Charizard', 'number': 6, 'type': ['Fire', 'Flying']},
+    {'name': 'Blastoise', 'number': 9, 'type': ['Water']},
+    {'name': 'Pikachu', 'number': 25, 'type': ['Electric']},
+    {'name': 'Dragonite', 'number': 149, 'type': ['Dragon', 'Flying']},
 ]
-for pokemon_info in pokemon_data:
-    pokemon = Pokemon(pokemon_info['nombre'], pokemon_info['numero'], pokemon_info['tipo'])
-    tree_name.insertar_pokemon(pokemon)
-    tree_number.insertar_pokemon(pokemon)
-    tree_type.insertar_pokemon(pokemon)
 
+for Datos in Pokemones:
+    pokemon = PokemonGeneracion(Datos['name'], Datos['number'], Datos['type'])
+    arbol_generaciones.insert_node(pokemon)
 
-print(tree_name.buscar_por_nombre('Bul')) 
-print(tree_type.obtener_tipo('planta'))
-print(tree_number.barrido_en_orden())
+pokemon_result = arbol_generaciones.search_by_name_number('bul', 1)
+if pokemon_result:
+    print(f"\nEl Pokemon buscado es:\n{pokemon_result}")
+print("\nNombres de Pokémon de Agua:")
+water = arbol_generaciones.get_names_by_type('Water')
+print(water)
+
+print("\nNombres de Pokémon de Fuego:")
+fire = arbol_generaciones.get_names_by_type('Fire')
+print(fire)
+
+print("\nNombres de Pokémon de Planta:")
+grass = arbol_generaciones.get_names_by_type('Grass')
+print(grass)
+
+print("\nNombres de Pokémon Electrico:")
+electric = arbol_generaciones.get_names_by_type('Electric')
+print(electric)
+
+print("\nListado de los pokemones:")
+arbol_generaciones.inorden()
+
+print("\nListado por nombre y nivel:")
+arbol_generaciones.by_level()
+pokemones_lista = ['Jolteon', 'Lycanroc', 'Tyrantrum']
+for nombre in pokemones_lista:
+    pokemones_iguales = arbol_generaciones.search_by_name_prefix(nombre.lower())
+    if pokemones_iguales:
+        print(f"\n{nombre}:")
+        for pokemon in pokemones_iguales:
+            print(pokemon)
+    else:
+        print(f"\nEl Pokémon {nombre} no fue encontrado.")
+electric_steel_count = arbol_generaciones.count_type_occurrences(['Electric', 'Steel'])
+print(f"\nLos Pokemones electricos/acero son: {electric_steel_count}")
